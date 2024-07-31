c1e812d2dc276986c3248bd5ea2cbe4c7939cffa
abdullah Al Omar <abdullahalomar048@gmail.com>
2024-08-01 00:37:18 +0600

    layout add

---
+++ src/app/(WihCommonLayout)/page.tsx
@@ -0,0 +1,10 @@
+const page = () => {
+  return (
+    <div>
+      <p>Welcome to my website</p>
+      <button className="btn btn-primary">Secondary</button>
+    </div>
+  );
+};
+
+export default page;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,22 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#EF5A6F",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "light",
+    ],
+  },
 };
 export default config;
