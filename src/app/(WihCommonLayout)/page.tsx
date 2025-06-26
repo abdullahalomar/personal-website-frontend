2565cad474cde3c2e2dbf5b8ac9e21f27e7389c3
abdullah Al Omar <abdullahalomar048@gmail.com>
2025-06-27 02:35:24 +0600

    some changes

--- src/app/(WihCommonLayout)/page.tsx
+++ src/app/(WihCommonLayout)/page.tsx
@@ -7,7 +7,6 @@ import ContactUsPage from "./contact-us/page";
 
 import Education from "@/components/UI/Education/Education";
 import Award from "@/components/UI/Award/Award";
-import Testimonial from "@/components/UI/Testimonial/Testimonial";
 import BlogsPage from "./blogs/page";
 import Experience from "@/components/UI/Experience/Experience";
 import AllSkills from "@/components/UI/AllSkills/AllSkills";
